ab0341ab5723a8a77eceb99aacb58d1d409da613
annakleszczynska <58387864+annakleszczynska@users.noreply.github.com>
2025-07-31 08:37:05 +0200

    Added sample files for integration

---
+++ www/ember_i18n_json_module.js
@@ -0,0 +1,22 @@
+export default {
+  "empty_string_key": "",
+  "with_description_key": "This key has a description in some file-formats.",
+  "with_line_break_key": "This key has a line break\nin it.",
+  "nested_key": {
+    "level_one_a": {
+      "level_two": "A deeply nested key."
+    },
+    "level_one_b": "A nested key."
+  },
+  "plural_key": {
+    "one": "Plural key one",
+    "other": "Plural key other",
+    "zero": "Plural key zero"
+  },
+  "array_key": [
+    "first entry",
+    "second entry",
+    "third entry"
+  ],
+  "simple_key": "Most basic translation."
+};
